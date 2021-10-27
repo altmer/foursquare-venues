3b0475d1c6b03bff20c05246986bdf80971cad05
Andrey Marchenko <andrei.marchenko@protonmail.com>
2021-10-27 13:50:45 +0200

    quotes service

---
+++ app/services/quotes.rb
@@ -0,0 +1,13 @@
+class QuoteOfTheDay
+  def initialize
+  end
+
+  def call
+    response = Faraday.get('https://quotes.rest/qod')
+    raise 'requesting quotes failed' if response.code >= 300
+
+    content = JSON.parse(response.body)
+    quote = content.dig('contents', 'quotes').first
+    quote.slice('quote', 'author')
+  end
+end
